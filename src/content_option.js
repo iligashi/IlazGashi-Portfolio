dcf733af61fd0766a0a5bb4bd37c7ddd22da75ee
iligashi <119267734+iligashi@users.noreply.github.com>
2025-07-16 00:29:05 +0200

    .

---
+++ src/content_option.js
@@ -0,0 +1,175 @@
+const logotext = "Ilaz";
+const meta = {
+    title: "Ilaz Gashi",
+    description: "I’m John Doe  Full stack devloper,currently working in Prishtina",
+};
+
+const introdata = {
+    title: "I’m Ilaz Gashi",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Enthusiastic junior developer skilled in full-stack development. Eager to contribute to collaborative projects and grow within a professional development team.",
+    your_img_url: "https://i.imgur.com/IKGUMS1.jpeg",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "Enthusiastic junior developer skilled in full-stack development with experience in HTML, CSS, JavaScript, and React. Eager to contribute to collaborative projects and grow within a professional development team.",
+};
+const worktimeline = [{
+        jobtitle: "Freelancer",
+        where: "Home",
+        date: "2020",
+    },
+    {
+        jobtitle: "Software Programming Instructor",
+        where: "Digital School",
+        date: "2024",
+    },
+    {
+        jobtitle: "Junior General Tech Engineer ",
+        where: "Digital School",
+        date: "2025",
+    },
+];
+
+const skills = [{
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "PHP",
+        value: 85,
+    },
+    {
+        name: "MSQL",
+        value: 80,
+    },
+    {
+        name: "JAVA",
+        value: 70,
+    },
+    {
+        name: "Bootstrap",
+        value: 80,
+    },
+    {
+        name: ".Net core",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "Websites",
+        description: " I create modern, responsive websites that deliver a seamless user experience.",
+    },
+    {
+        title: "Management Systems",
+        description: "I develop efficient management systems to streamline business operations and improve productivity.",
+    },
+    {
+        title: "Apps",
+        description: "I build user-friendly applications tailored to your needs, ensuring functionality and great performance.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://sdmntpreastus2.oaiusercontent.com/files/00000000-3fe8-51f6-bdcc-026af46335bb/raw?se=2025-03-30T20%3A29%3A55Z&sp=r&sv=2024-08-04&sr=b&scid=66ef4d5b-9ebf-5198-a405-2780c7789459&skoid=2f36945c-3adc-4614-ac2b-eced8f672c58&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-03-30T01%3A22%3A14Z&ske=2025-03-31T01%3A22%3A14Z&sks=b&skv=2024-08-04&sig=HbGJQegacNGKksLL5LxneKYyKez14qtTWDpPGd9REfk%3D",
+        description: "Real Estate Marketplace, Technologies: React (TypeScript), .NET, MySQL, Bootstrap",
+        link: "https://github.com/iligashi/Abode.git",
+    },
+    {
+        img: "https://sdmntpreastus2.oaiusercontent.com/files/00000000-a038-51f6-a395-7e8416eb8f1a/raw?se=2025-03-30T20%3A38%3A33Z&sp=r&sv=2024-08-04&sr=b&scid=23121af5-a055-5509-a534-a61f04e4c458&skoid=2f36945c-3adc-4614-ac2b-eced8f672c58&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-03-30T01%3A21%3A48Z&ske=2025-03-31T01%3A21%3A48Z&sks=b&skv=2024-08-04&sig=6yM%2BAbxZI0JsTljQwlzCY8qUZk6AyCVZb6psKRl%2B64M%3D",
+        description: "Personal Portfolio, Developed a personal website using React and Bootstrap to showcase projects and skills",
+        link: "https://github.com/iligashi",
+     },
+    {
+        img: "https://sdmntpreastus2.oaiusercontent.com/files/00000000-a038-51f6-a395-7e8416eb8f1a/raw?se=2025-03-30T20%3A38%3A33Z&sp=r&sv=2024-08-04&sr=b&scid=23121af5-a055-5509-a534-a61f04e4c458&skoid=2f36945c-3adc-4614-ac2b-eced8f672c58&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-03-30T01%3A21%3A48Z&ske=2025-03-31T01%3A21%3A48Z&sks=b&skv=2024-08-04&sig=6yM%2BAbxZI0JsTljQwlzCY8qUZk6AyCVZb6psKRl%2B64M%3D",
+        description: "QR Code Generator, Technologies: HTML, CSS, JavaScript, PHP, MySQL",
+        link: "https://github.com/iligashi/QRCODE_GENERATOR.git",
+    },
+    //  {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://sdmntpreastus2.oaiusercontent.com/files/00000000-a038-51f6-a395-7e8416eb8f1a/raw?se=2025-03-30T20%3A38%3A33Z&sp=r&sv=2024-08-04&sr=b&scid=23121af5-a055-5509-a534-a61f04e4c458&skoid=2f36945c-3adc-4614-ac2b-eced8f672c58&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-03-30T01%3A21%3A48Z&ske=2025-03-31T01%3A21%3A48Z&sks=b&skv=2024-08-04&sig=6yM%2BAbxZI0JsTljQwlzCY8qUZk6AyCVZb6psKRl%2B64M%3D",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://sdmntpreastus2.oaiusercontent.com/files/00000000-a038-51f6-a395-7e8416eb8f1a/raw?se=2025-03-30T20%3A38%3A33Z&sp=r&sv=2024-08-04&sr=b&scid=23121af5-a055-5509-a534-a61f04e4c458&skoid=2f36945c-3adc-4614-ac2b-eced8f672c58&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-03-30T01%3A21%3A48Z&ske=2025-03-31T01%3A21%3A48Z&sks=b&skv=2024-08-04&sig=6yM%2BAbxZI0JsTljQwlzCY8qUZk6AyCVZb6psKRl%2B64M%3D",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // }
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ilazgashi258@gmail.com",
+    YOUR_FONE: "(383)49 732 127",
+    description: "Always ready to response at any time. If you have any questions about me contact me. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_h3ral8d",
+    YOUR_TEMPLATE_ID: "template_0dzx88f",
+    YOUR_USER_ID: "XjDQ16Moq4IOUH77j",
+};
+
+const socialprofils = {
+    github: "https://github.com/iligashi",
+    facebook: "https://facebook.com/IlazGashi",
+    linkedin: "linkedin.com/in/ilaz-gashi-a2b868259/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
